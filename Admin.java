a34217e34daafccdd91a377dc1f5bbfc9e4245cc
Your Name <mudaurotondwaagriment2007@gmail.com>
2026-05-05 08:40:20 +0200

    Admin

---
+++ Admin.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.admin;
+
+/**
+ *
+ * @author mudau
+ */
+public class Admin {
+
+    public static void main(String[] args) {
+                java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NewJFrame().setVisible(true);
+            }
+        });
+    }
+}
